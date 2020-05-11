07b22bb1dfdcda309dd347799b40c139f8daef30
piedaholic <harsh113300@nitp.ac.in>
2020-05-12 00:28:26 +0200

    Added methods to print args and kwargs

--- src/pyutils/root_utils.py
+++ src/pyutils/root_utils.py
@@ -6,3 +6,12 @@ def nvl(a,b):
     if isinstance(a, str) and a == '':
         return b
     return a
+
+def print_kwargs(**kwargs):
+    for key, value in kwargs.items():
+        print ("%s == %s" %(key, value))
+
+def print_args(*argv):
+    count = 0
+    for arg in argv:
+        print("arg[" + str(count) + "]:", arg)
